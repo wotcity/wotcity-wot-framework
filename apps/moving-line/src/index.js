341a552027919ff7656fb1454125a046b1e849a7
Jollen Chen <jollen@moko365.com>
2016-02-26 20:15:42 +0800

    Fix the Copyright legal name

--- apps/moving-line/src/index.js
+++ apps/moving-line/src/index.js
@@ -1,7 +1,8 @@
 /**
  *
- *  .CITY Starter Kit
- *  Copyright 2015 WoT.City Inc. All rights reserved.
+ *  Starter Kit / WoT.City Open Source Project
+ * 
+ *  Copyright 2015 WoT.City Open Source Project
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- tests/coap-view-data.js
+++ tests/coap-view-data.js
@@ -1,6 +1,6 @@
 var coap = require('coap');
 
-var clientWriable = coap.request('coap://localhost:8000/object/testman/viewer');
+var clientWriable = coap.request('coap://127.0.0.1:8000/object/5550937980d51931b3000009/viewer');
 
 clientWriable.on('response', function(res) {
     res.pipe(process.stdout)
