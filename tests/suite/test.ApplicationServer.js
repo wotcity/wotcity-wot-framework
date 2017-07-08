347ec1ff37724797901140a56740bd2bfd3254cf
Jollen Chen <jollen@moko365.com>
2017-07-09 07:51:23 +0800

    update test suite, #29

--- tests/suite/test.ApplicationServer.js
+++ tests/suite/test.ApplicationServer.js
@@ -1,22 +1,38 @@
 var vows = require('vows'),
     assert = require('assert');
 
-var server = require('../../servers/websocket-broker');
-
 // Environment
 process.env['PORT'] = 9000;
-
-var onstart = function(payload) {
-    server.shutdown(function() {
-    });    
-}
 
 vows.describe('WoT.City Framework Tests').addBatch({
     'Testing WebSocket Broker Application Server': {
         'is server started': function () {
+            var server = require('../../servers/websocket-broker');
+
+            var onstart = function(payload) {
+                server.shutdown(function() {
+                    return 0;
+                });    
+            };
+
             server.start({
                 onstart: onstart
             });
         }
-    }
+    },
+    'Testing CoAP Broker Application Server': {
+        'is server started': function () {
+            var server = require('../../servers/coap-broker'); 
+
+            var onstart = function(payload) {
+                server.shutdown(function() {
+                    return 0;
+                });    
+            };
+
+            server.start({
+                onstart: onstart
+            });
+        }
+    }    
 }).export(module);;
